de59fce8f42af4cc17aad503adcb4b8a5fe7364e
Rodrigo <rodrigo-gomes1302@outlook.com>
2025-06-14 05:46:01 -0300

    Deploy 2

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 # Porta do servidor e sinalizacao de debug para o Flask 
 PORT = int(os.getenv("PORT", 5000))
-DEBUG = os.getenv("FLASK_DEBUG", "false").lower()
+DEBUG = os.getenv("FLASK_DEBUG")
 
 # Geminic Key
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from bot.whatsapp import app
 from config.settings import DEBUG, PORT
-print(DEBUG)
+
 if __name__ == "__main__":
-    app.run(debug=DEBUG, port=PORT)
+    app.run(host="0.0.0.0", port=PORT, debug=False)
